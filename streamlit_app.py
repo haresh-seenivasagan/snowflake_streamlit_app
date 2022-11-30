b9aad2455b7e23ecaeffb92985c01e705c51d408
Haresh Seenivasagan <75823911+haresh-seenivasagan@users.noreply.github.com>
2022-11-30 14:49:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("hahaha this is a test")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
